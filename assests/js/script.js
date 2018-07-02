2b3a5614bacd3459355ea7e33379461a7b21fbf5
@shubham860 <shubham.chauhan860@gmail.com>
2018-07-02 14:29:09 +0530

    up6

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,21 @@
+var exp =  document.getElementById("ip").value;
+
 function display(n1){
-   document.getElementById("ip").value += n1;
+   document.getElementById("ip").value = document.getElementById("ip").value + n1;
+}
+
+function equal(){
+  var exp =  document.getElementById("ip").value;
+  if(exp){
+    document.getElementById("ip").value = eval(exp);
+}
+}
+
+function clean(){
+   document.getElementById("ip").value = "";
+}
+
+function back(){
+  var exp =  document.getElementById("ip").value;
+  document.getElementById("ip").value = exp.substring(0,exp.length-1);
 }
